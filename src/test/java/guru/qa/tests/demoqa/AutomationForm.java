a079610dd86bde7dd022ca6e2d14afcc21a9354f
Nikita Miroshnichenko <Niki.tone@yandex.ru>
2022-03-27 21:49:10 +0500

    added selenoid config

--- src/test/java/guru/qa/tests/demoqa/AutomationForm.java
+++ src/test/java/guru/qa/tests/demoqa/AutomationForm.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 
@@ -19,6 +20,11 @@ public class AutomationForm {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC",true);
+        capabilities.setCapability("enableVideo",true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @Test
